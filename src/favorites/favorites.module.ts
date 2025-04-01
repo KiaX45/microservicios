ed95735edc661135a97f7ca955633cb9a778abaa
LuisMedina <luisalejandromedinab@gmail.com>
2025-03-31 22:04:14 -0500

    correción de favorites Crud

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
 import { PrismaService } from 'src/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { ProductsService } from 'src/products/products.service';
+import { CategoriesService } from 'src/categories/categories.service';
+
 
 @Module({
   controllers: [FavoritesController],
-  providers: [FavoritesService, PrismaService],
+  providers: [FavoritesService, PrismaService, UsersService, ProductsService, CategoriesService],
 })
 export class FavoritesModule {}
